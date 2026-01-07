28c192b5ba81e80858756102a446837d6e812626
shivam04 <sinhashivam04@gmail.com>
2026-01-08 01:11:18 +0530

    LCEL

---
+++ 4-LCEL/client.py
@@ -0,0 +1,30 @@
+import requests
+import streamlit as st
+
+
+def get_groq_response(input_text):
+    print(input_text)
+    json_body={
+      "input": {
+        "language": "French",
+        "text": input_text
+      },
+      "config": {},
+      "kwargs": {
+        "additionalProp1": {}
+      }
+    }
+    print(json_body)
+    response=requests.post("http://127.0.0.1:8000/chain/invoke",json_body)
+
+    print(response.json())
+
+
+    return response.json()
+
+## Streamlit app
+st.title("LLM Application Using LCEL")
+input_text=st.text_input("Enter the text you want to convert to french")
+
+if input_text:
+    st.write(get_groq_response(input_text))
