cc896b3b1f628777cb8678e9ceb058b54f48420c
shivam04 <sinhashivam04@gmail.com>
2026-01-10 01:31:41 +0530

    Search Engine

---
+++ 9-SearchEngine/app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun
+from langchain.agents import create_agent
+from langchain_groq import ChatGroq
+
+
+## Arxiv and wikipedia Tools
+arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
+wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
+
+search=DuckDuckGoSearchRun(name="Search")
+
+st.title("🔎 LangChain - Chat with search")
+
+## Sidebar for settings
+st.sidebar.title("Settings")
+api_key=st.sidebar.text_input("Enter your Groq API Key:",type="password")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"]=[
+        {"role":"assistant","content":"Hi,I'm a chatbot who can search the web. How can I help you?"}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+if prompt:=st.chat_input(placeholder="What is machine learning?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    llm = ChatGroq(groq_api_key=api_key, model_name="llama-3.1-8b-instant", streaming=True)
+    tools = [search, arxiv, wiki]
+
+    search_agent = create_agent(
+        model=llm,
+        tools=tools
+    )
+
+    with st.chat_message("assistant"):
+        response = search_agent.invoke({
+            "messages": [
+                {"role": "user", "content": "Tell me about Langsmith"}
+            ]
+        })
+        st.session_state.messages.append({'role': 'assistant', "content": response["messages"][-1].content})
+        st.write(response)
